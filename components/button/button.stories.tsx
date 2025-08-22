a8787d1b097a6fa05f863b27ae578e69abd7be39
min-s9709 <mim970929@naver.com>
2025-08-22 18:53:42 +0900

    init [#1] : storybook 설치 및 세팅 -rename: Button.stories.ts -> button.stories.tsx

--- components/button/button.stories.tsx
+++ components/button/button.stories.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/Button",
+  title: "버튼 컴포넌트 예시",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
